93dd9d60a43054592fccca8731e4366fa36d1f4d
Andryprasetia <andreprasetia83@gmail.com>
2022-07-04 00:32:55 +0700

    Jenis Hewan APP

--- JenisHewan/app/src/main/java/com/andryprasetia/jenishewan/MainActivity.kt
+++ JenisHewan/app/src/main/java/com/andryprasetia/jenishewan/MainActivity.kt
@@ -2,10 +2,30 @@ package com.andryprasetia.jenishewan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.andryprasetia.jenishewan.adapter.AdapterHewan
+import com.andryprasetia.jenishewan.databinding.ActivityMainBinding
+import com.andryprasetia.jenishewan.model.Hewan
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val listHewan = ArrayList<Hewan>()
+        listHewan.add(Hewan("Rubah",R.drawable.rubah,"Karnivora","Vivipar"))
+        listHewan.add(Hewan("Singa",R.drawable.singa,"Karnivora","Vivipar"))
+        listHewan.add(Hewan("Kelinci",R.drawable.kelinci,"Herbivora","Vivipar"))
+        listHewan.add(Hewan("Beruang",R.drawable.beruang,"Karnivora","Vivipar"))
+        listHewan.add(Hewan("Anjing",R.drawable.anjing,"Karnivora","Vivipar"))
+
+        binding.list.adapter = AdapterHewan(this,listHewan,object :AdapterHewan.OnClickListener {
+            override fun detailData(item: Hewan?) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
     }
 }
