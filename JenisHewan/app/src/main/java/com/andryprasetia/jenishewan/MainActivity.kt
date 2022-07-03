bd643316b4dc6c7fcc3546208a8b10d2d69ad390
Andryprasetia <andreprasetia83@gmail.com>
2022-07-03 22:25:46 +0700

    Jenis Hewan APP

---
+++ JenisHewan/app/src/main/java/com/andryprasetia/jenishewan/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.andryprasetia.jenishewan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
